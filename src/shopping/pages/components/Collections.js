6374508c33a6e3f039fe586e4976ade6d4354cba
Neehal16 <your_email@example.com>
2024-11-21 21:08:25 +0530

    first commit
    main

---
+++ src/shopping/pages/components/Collections.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Collections = () => {
+    return(
+   <div className="collections">
+    <h3>Mens Collections</h3>
+    <br/>
+  <div className="mens">
+   
+<img src="assets/project photos/Mens/download.jpeg" alt="hah" />
+<img src="assets/project photos/Mens/download (2).jpeg" alt="hah" />
+<img src="assets/project photos/Mens/download (3).jpeg" alt="hah" />
+<img src="assets/project photos/Mens/download (4).jpeg" alt="hah" />
+<img src="assets/project photos/Mens/images.jpeg" alt="hah" />
+<img src="assets/project photos/Mens/images (2).jpeg" alt="hah" />
+  </div>
+<br/>
+  <h3>Womens Collections</h3>
+  <br/>
+  <div className="womens">
+
+<img src="assets/project photos/Womens/1.jpeg" alt="hah" />
+<img src="assets/project photos/Womens/2.jpeg" alt="hah" />
+<img src="assets/project photos/Womens/3.jpeg" alt="hah" />
+
+<img src="assets/project photos/Womens/5.jpeg" alt="hah" />
+
+<img src="assets/project photos/Womens/7.jpeg" alt="hah" />
+<img src="assets/project photos/Womens/9.jpeg" alt="hah" />
+
+
+  </div>
+   </div>
+    )
+}
+export default Collections;
